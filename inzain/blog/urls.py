272b9984db968801dfc62503d0379a504a94488a
Zain Memon <zain@inzain.net>
2009-11-02 01:38:43 -0800

    fixing all broken links

--- inzain/blog/urls.py
+++ inzain/blog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(r'^$', 'inzain.blog.views.index', name='blog-index'),
     url(r"^archive/$", 'inzain.blog.views.archive', name="blog-archive"),
-    url(r"^feeds/$", 'django.views.generic.simple.direct_to_template', 
-        dict(template='feeds.html'), name="blog-feeds"),
+    url(r'^search/$', 'django.views.generic.simple.direct_to_template', 
+        dict(template="comingsoon.html")),
     url(r"^(?P<slug>[-\w]+)/", 'inzain.blog.views.blogpost', name="blog-post"),
 )
--- inzain/urls.py
+++ inzain/urls.py
@@ -8,6 +8,12 @@ urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.direct_to_template', dict(template="about.html")),
     (r'^admin/', include(admin.site.urls)),
     (r'^blog/', include('inzain.blog.urls')),
+    (r'^links/$', 'django.views.generic.simple.direct_to_template', 
+        dict(template="comingsoon.html")),
+    (r'^projects/$', 'django.views.generic.simple.direct_to_template', 
+        dict(template="comingsoon.html")),
+    (r"^feeds/$", 'django.views.generic.simple.direct_to_template', 
+        dict(template='feeds.html')),
 )
 
 if settings.LOCAL_DEV:
